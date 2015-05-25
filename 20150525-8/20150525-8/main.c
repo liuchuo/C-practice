39569680ce429aa5f8cc74f0296f345720afc978
liuchuo <95323362@qq.com>
2015-05-25 21:59:17 +0800

    "%-10s"

---
+++ 20150525-8/20150525-8/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  20150525-8
+//
+//  Created by 欣 陈 on 15/5/25.
+//  Copyright (c) 2015年 欣 陈. All rights reserved.
+//
+
+//"-10s"
+#include <stdio.h>
+int main(){
+    char s[]="abcdef";
+    printf("%10s%-10s\n%-10s%-10s\n%10s%10s",s,s,s,s,s,s);
+    return 0;
+    
+}
