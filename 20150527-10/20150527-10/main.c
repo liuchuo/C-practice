8e482500841bb08fc863dcc9ca7a9abcc16a9ed8
liuchuo <95323362@qq.com>
2015-05-27 22:09:18 +0800

    头文件

---
+++ 20150527-10/20150527-10/main.c
@@ -0,0 +1,26 @@
+//
+//  main.c
+//  20150527-10
+//
+//  Created by 欣 陈 on 15/5/27.
+//  Copyright (c) 2015年 欣 陈. All rights reserved.
+//
+
+//#include <stdio.h>
+#include "format.h"
+
+int main(){
+//    int a,b,c,d;
+//    char string[] = "CHINA";
+//    a = 1;
+//    b = 2;
+//    c = 3;
+//    d = 4;
+//    PR(D1,a);
+//    PR(D2,a,b);
+//    PR(D3,a,b,c);
+//    PR(D4,a,b,c,d);
+//    PR(S,string);
+    f();
+    return 0;
+}
